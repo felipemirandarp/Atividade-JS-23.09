2b6c4970f216b4d36bddfa12675e178bef6b2e12
Felipe Miranda <felipemirandaresendepinheiro@gmail.com>
2024-09-23 15:32:16 -0300

    Add files via upload

---
+++ somatorio.js
@@ -0,0 +1,8 @@
+let num = parseInt(prompt("Digite um numero "))
+let i = 0
+
+do{
+    resultado +=num 
+    
+}while ( num != 0)
+console.log(" "+resultado)
---
+++ tabuada2.js
@@ -0,0 +1,16 @@
+
+let num  = 0
+let result 
+
+while (num <= 9  ){
+    tabuada = 0
+
+    while(tabuada <= 10){
+       
+        console.log(num*tabuada)
+        tabuada++
+        
+    }
+    num++
+    console.log('')
+}
